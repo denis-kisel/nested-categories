0d4de7deaf057141601bfc0023b2cb2df6b62d1b
vagrant <vagrant@homestead>
2022-06-15 12:58:50 +0000

    fix composer

--- src/Commands/NestedCategoryInstallCommand.php
+++ src/Commands/NestedCategoryInstallCommand.php
@@ -5,5 +5,11 @@ namespace DenisKisel\NestedCategory\Commands;
 
 class NestedCategoryInstallCommand extends \Illuminate\Console\Command
 {
+    protected $signature = 'nested-category:install';
+    protected $description = 'Nested Category Install';
 
+    public function handle()
+    {
+
+    }
 }
